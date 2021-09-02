45fa64a96762c51f54823f0b44fc59d52abecd05
Georjane <pzgeorjane@gmail.com>
2021-09-02 15:13:55 +0100

    Add tests

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+    # model association
+    has_many :bookcategories, dependent: :destroy
+
+    # validations
+    validates_presence_of :title, :author, :category
 end
