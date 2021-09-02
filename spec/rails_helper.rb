583f79524b2500aa8df5147b26760ee7e7219c4a
Georjane <pzgeorjane@gmail.com>
2021-09-02 16:05:54 +0100

    Add request tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -90,3 +90,11 @@ RSpec.configure do |config|
   end
   # [...]
 end
+# [...]
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+# [...]
+RSpec.configuration do |config|
+  # [...]
+  config.include RequestSpecHelper, type: :request
+  # [...]
+end
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 
 RSpec.describe 'Books API', type: :request do
   # initialize test data
-  let!(:books) { create_list(:todo, 10) }
-  let(:todo_id) { books.first.id }
-
+  let!(:books) { create_list(:book, 10) }
+  let(:book_id) { books.first.id }
+  let(:name) { "simple name" }
   # Test suite for GET /books
   describe 'GET /books' do
     # make HTTP get request before each example
@@ -24,12 +24,12 @@ RSpec.describe 'Books API', type: :request do
 
   # Test suite for GET /books/:id
   describe 'GET /books/:id' do
-    before { get "/books/#{todo_id}" }
+    before { get "/books/#{book_id}" }
 
     context 'when the record exists' do
-      it 'returns the todo' do
+      it 'returns the book' do
         expect(json).not_to be_empty
-        expect(json['id']).to eq(todo_id)
+        expect(json['id']).to eq(book_id)
       end
 
       it 'returns status code 200' do
@@ -38,14 +38,14 @@ RSpec.describe 'Books API', type: :request do
     end
 
     context 'when the record does not exist' do
-      let(:todo_id) { 100 }
+      let(:book_id) { 100 }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
       end
 
       it 'returns a not found message' do
-        expect(response.body).to match(/Couldn't find Todo/)
+        expect(response.body).to match(/Couldn't find book/)
       end
     end
   end
@@ -58,7 +58,7 @@ RSpec.describe 'Books API', type: :request do
     context 'when the request is valid' do
       before { post '/books', params: valid_attributes }
 
-      it 'creates a todo' do
+      it 'creates a book' do
         expect(json['title']).to eq('Learn Elm')
       end
 
@@ -86,7 +86,7 @@ RSpec.describe 'Books API', type: :request do
     let(:valid_attributes) { { title: 'Shopping' } }
 
     context 'when the record exists' do
-      before { put "/books/#{todo_id}", params: valid_attributes }
+      before { put "/books/#{book_id}", params: valid_attributes }
 
       it 'updates the record' do
         expect(response.body).to be_empty
@@ -100,7 +100,7 @@ RSpec.describe 'Books API', type: :request do
 
   # Test suite for DELETE /books/:id
   describe 'DELETE /books/:id' do
-    before { delete "/books/#{todo_id}" }
+    before { delete "/books/#{book_id}" }
 
     it 'returns status code 204' do
       expect(response).to have_http_status(204)
